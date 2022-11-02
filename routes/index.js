64907a6e996fdb36e59c748dd44cebe4a240fa3f
Andy Qin <56485866+AndyQYH@users.noreply.github.com>
2022-11-01 20:46:25 -0700

    Added Google Auth

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,7 @@ let bodyParser = require('body-parser')
 let cookieParser = require('cookie-parser')
 let dotenv = require('dotenv')
 const User = require('../models/User')
+const { ensureAuth, ensureGuest } = require('../middleware/auth')
 
 dotenv.config()
 
@@ -13,36 +14,20 @@ router.use(cookieParser())
 router.use(bodyParser.json())
 router.use(express.static('public'))
 
-router.get('/',(req ,res)=>{
+router.get('/', ensureGuest,(req ,res)=>{
     res.render('index',{
         msg:'index',
+        userinfo:'',
         key:api_key
     })
 })
 
-
-const {OAuth2Client} = require('google-auth-library');
-const client = new OAuth2Client('524569409435-hmbta1vbvgdpfmvfq2m8h5a6df6bv7t5.apps.googleusercontent.com');
-
-router.post('/signIn',async (req ,res)=>{
-    console.log("data received:")
-    console.log(req.body)
-    try{
-        const ticket = await client.verifyIdToken({
-            idToken: req.body.credential,
-            audience: '524569409435-hmbta1vbvgdpfmvfq2m8h5a6df6bv7t5.apps.googleusercontent.com',
-        });
-        const payload = ticket.getPayload();
-        const email = payload['email'];
-        console.log("email:" + email);
-        let data = email
-
-        res.status(200).send(data)
-    }catch (err){
-        console.error(err)
-        res.status(400).send('invalid sign in, please try again')
-    }
-    
+router.get("/user",ensureAuth, async(req,res)=>{
+    res.render('index',{
+        msg:"user",
+        userinfo:req.user,
+        key:api_key
+    })
 })
 
 module.exports = router
--- server.js
+++ server.js
@@ -1,18 +1,20 @@
-var express = require('express')
-var dotenv = require('dotenv')
-var expressLayouts = require('express-ejs-layouts')
-var bodyParser = require('body-parser')
-var methodOverride = require('method-override')
-//var sqliteDB = require('./models/db')
-var scheduleRouter = require('./routes/map')
-var indexRouter = require('./routes/index')
-var userRouter = require('./routes/users')
-
+let express = require('express')
+let dotenv = require('dotenv')
+let expressLayouts = require('express-ejs-layouts')
+let bodyParser = require('body-parser')
+let methodOverride = require('method-override')
+let passport = require('passport')
+const session = require('express-session')
+const {setup} = require('./config/passport')
+passport = setup(passport)
+let scheduleRouter = require('./routes/map')
+let indexRouter = require('./routes/index')
+let userRouter = require('./routes/users')
+let authRouter = require('./routes/auth')
 
 if (process.env.NODE_ENV !== 'production') {
     dotenv.config()
 }
-
 
 const app = express()
 const PORT= parseInt(process.env.PORT);
@@ -28,17 +30,32 @@ app.use(express.static('public'))
 app.use(express.static('node_modules'))
 app.use(bodyParser.urlencoded({ extended: true }))
 
+app.use(
+    session({
+      secret: 'campus scheduler SCU',
+      resave: false,
+      saveUninitialized: false
+    })
+)
+
+// Passport middleware
+app.use(passport.initialize())
+app.use(passport.session())
+
 const mongoose = require('mongoose')
-mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true })
+mongoose.connect(process.env.DATABASE_URL, { 
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    family: 4 
+ })
 const db = mongoose.connection
 db.on('error', error => console.error(error))
 db.once('open', () => console.log('Connected to Mongoose'))
 
-
 app.use('/',indexRouter)
 app.use('/log-in',userRouter)
 app.use('/schedule', scheduleRouter)
-//app.use('/sessions',sessionRouter)
+app.use('/auth',authRouter)
 
 
 app.listen(PORT, ()=>{

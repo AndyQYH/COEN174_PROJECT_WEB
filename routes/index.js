6f157723bf7177fa4f9f41e159162dd6301be713
junhe cui <jcui3@linux11105.dc.engr.scu.edu>
2022-11-02 14:58:41 -0700

    fetch

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,7 @@ let cookieParser = require('cookie-parser')
 let dotenv = require('dotenv')
 const User = require('../models/User')
 const { ensureAuth, ensureGuest } = require('../middleware/auth')
-
+const webpage = "https://ecampus.scu.edu/psc/csprd92/EMPLOYEE/SA/c/SA_LEARNER_SERVICES.SSR_SSENRL_SCHD_W.GBL"
 dotenv.config()
 
 const api_key = process.env.GOOGLE_KEY
@@ -29,5 +29,27 @@ router.get("/user",ensureAuth, async(req,res)=>{
         key:api_key
     })
 })
+
+fetch(webpage)
+    .then(function(response) {
+        // When the page is loaded convert it to text
+        return response.text()
+    })
+    .then(function(html) {
+        // Initialize the DOM parser
+        var parser = new DOMParser();
+
+        // Parse the text
+        var doc = parser.parseFromString(html, "text/html");
+
+        // You can now even select part of that html as you would in the regular DOM 
+        // Example:
+        // var docArticle = doc.querySelector('article').innerHTML;
+
+        console.log(doc);
+    })
+    .catch(function(err) {  
+        console.log('Failed to fetch page: ', err);  
+    });
 
 module.exports = router

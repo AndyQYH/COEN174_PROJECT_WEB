df509faea64e9027f055289539ad6936fb68755a
oooZCooo <1055298163@qq.com>
2022-11-02 22:53:22 -0700

    setup access to ecampus elements

--- routes/index.js
+++ routes/index.js
@@ -4,6 +4,7 @@ let cookieParser = require('cookie-parser')
 let dotenv = require('dotenv')
 const User = require('../models/User')
 const { ensureAuth, ensureGuest } = require('../middleware/auth')
+const webpageLogin = "https://ecampus.scu.edu/psp/csprd92/?cmd=login";
 const webpage = "https://ecampus.scu.edu/psc/csprd92/EMPLOYEE/SA/c/SA_LEARNER_SERVICES.SSR_SSENRL_SCHD_W.GBL"
 dotenv.config()
 
@@ -11,6 +12,7 @@ const api_key = process.env.GOOGLE_KEY
 console.log(api_key)
 const router = express.Router()
 router.use(cookieParser())
+router.use(bodyParser.urlencoded({extended : true}))
 router.use(bodyParser.json())
 router.use(express.static('public'))
 
@@ -30,26 +32,46 @@ router.get("/user",ensureAuth, async(req,res)=>{
     })
 })
 
-fetch(webpage)
-    .then(function(response) {
-        // When the page is loaded convert it to text
-        return response.text()
-    })
-    .then(function(html) {
-        // Initialize the DOM parser
-        var parser = new DOMParser();
+router.post('/getData',async (req ,res)=>{
+  console.log(req.body)
 
-        // Parse the text
-        var doc = parser.parseFromString(html, "text/html");
+  const puppeteer = require('puppeteer')
+  const loggedCheck = async (page) => {
+      try {
+          await page.waitForSelector('#pt_envinfo', { timeout: 10 });
+          return true;
+      } catch(err) {
+          return false;
+      }
+  };
 
-        // You can now even select part of that html as you would in the regular DOM 
-        // Example:
-        // var docArticle = doc.querySelector('article').innerHTML;
 
-        console.log(doc);
-    })
-    .catch(function(err) {  
-        console.log('Failed to fetch page: ', err);  
-    });
+  const browser = await puppeteer.launch();
+  const page = await browser.newPage();
 
+  let isLogged = false;
+
+  if (!isLogged) {
+      console.log(`Try to log in.`);
+      await page.goto(webpageLogin);
+      //console.log(await page.content())
+      await page.type('#userid', req.body.username);
+      await page.type('#pwd', req.body.password);
+      await page.waitForSelector('input[name=Submit]');
+      await page.click('input[name=Submit]');
+      await page.waitForNavigation();
+      isLogged = await loggedCheck(page);
+  }
+
+  if (!isLogged) {
+      throw new Error('Incorrect username or password.')
+  }
+
+  await page.goto(webpage)
+
+  console.log(await page.content())
+
+  await browser.close();
+
+})
 module.exports = router

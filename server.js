6bf4f744537d7804efbd9d2aada022a24db4d039
bananamtieu <emmett.tieu0905@gmail.com>
2022-11-22 23:33:31 -0800

    small update on nav

--- server.js
+++ server.js
@@ -7,8 +7,9 @@ let passport = require('passport')
 const session = require('express-session')
 const {setup} = require('./config/passport')
 passport = setup(passport)
-let scheduleRouter = require('./routes/map')
+let mapRouter = require('./routes/map')
 let indexRouter = require('./routes/index')
+//let scheduleRouter = require('./routes/schedule')
 let userRouter = require('./routes/users')
 let authRouter = require('./routes/auth')
 
@@ -54,7 +55,8 @@ db.once('open', () => console.log('Connected to Mongoose'))
 
 app.use('/',indexRouter)
 app.use('/log-in',userRouter)
-app.use('/schedule', scheduleRouter)
+//app.use('/schedule',scheduleRouter)
+app.use('/map', mapRouter)
 app.use('/auth',authRouter)
 
 
